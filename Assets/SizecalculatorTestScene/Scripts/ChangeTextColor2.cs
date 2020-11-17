e63d333742b016e7182100bb4f44373a2283f3ab
Erno Oljakka <erno.oljakka@gmail.com>
2020-11-17 11:52:18 +0200

    added test scene for sizecalculator

---
+++ Assets/SizecalculatorTestScene/Scripts/ChangeTextColor2.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ChangeTextColor2 : MonoBehaviour
+{
+    TMP_Text textColor;
+
+    void Start()
+    {
+        textColor = GetComponent<TMP_Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float r = Random.Range(0.1f, 1.0f);
+        textColor.color = new Color(r, r, r, 1.0f);
+    }
+}
---
+++ Assets/SizecalculatorTestScene/Scripts/PlayerMovement.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement2 : MonoBehaviour
+{
+
+    public float mainSpeed = 10.0f;
+    public float shiftAdd = 25.0f;
+    public float maxShift = 100.0f;
+    public float camSens = 0.15f;
+
+    private Vector3 lastMouse = new Vector3(255, 255, 255);
+    private float totalRun = 1.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        lastMouse = Input.mousePosition - lastMouse;
+        lastMouse = new Vector3(-lastMouse.y * camSens, lastMouse.x * camSens, 0);
+        lastMouse = new Vector3(transform.eulerAngles.x + lastMouse.x, transform.eulerAngles.y + lastMouse.y, 0);
+        transform.eulerAngles = lastMouse;
+        lastMouse = Input.mousePosition;
+
+        Vector3 direction = GetBaseInput();
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            totalRun += Time.deltaTime;
+            direction *= totalRun * shiftAdd;
+            direction.x = Mathf.Clamp(direction.x, -maxShift, maxShift);
+            direction.y = Mathf.Clamp(direction.x, -maxShift, maxShift);
+            direction.z = Mathf.Clamp(direction.x, -maxShift, maxShift);
+        }
+        else 
+        {
+            totalRun = Mathf.Clamp(totalRun * 0.5f, 1f, 1000f);
+            direction *= mainSpeed;
+        }
+
+        direction *= Time.deltaTime;
+        transform.Translate(direction);
+        
+    }
+        private Vector3 GetBaseInput()
+        {
+           Vector3 direction = new Vector3();
+
+            // Forward
+            if (Input.GetKey(KeyCode.W))
+                direction += new Vector3(0, 0, 1);
+
+            if (Input.GetKey(KeyCode.S))
+                direction += new Vector3(0, 0, -1);
+            
+            if (Input.GetKey(KeyCode.A))
+                direction += new Vector3(-1, 0, 0);
+            
+            if (Input.GetKey(KeyCode.D))
+                direction += new Vector3(1, 0, 0);
+
+            if (Input.GetKey(KeyCode.Space))
+                direction += new Vector3(0, 1, 0);
+
+            if (Input.GetKey(KeyCode.LeftControl))
+                direction += new Vector3(0, -1, 0);
+            
+            return direction;
+        }
+}
