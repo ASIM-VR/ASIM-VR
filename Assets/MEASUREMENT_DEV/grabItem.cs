00e32086db64571d0a09b2c2fc9470fe0915a8b7
Lassi Palojärvi <lassi.palojarvi@outlook.com>
2020-11-18 20:46:43 +0200

    GrabItem script and mockdisplay

---
+++ Assets/MEASUREMENT_DEV/grabItem.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class grabItem : MonoBehaviour
+{
+   
+    public Transform dest;
+    public bool grabbing = false;
+
+    private void Update() {
+
+        if (Input.GetKeyDown("f")){
+            grabbing = !grabbing;
+        }
+
+        if (grabbing){
+         GetComponent<Rigidbody>().useGravity = false;
+         this.transform.position = dest.position;
+         this.transform.parent = GameObject.Find("RightHand Controller").transform;
+        } else {
+            this.transform.parent = null;
+            GetComponent<Rigidbody>().useGravity = true;
+        }
+        
+     }
+
+}
