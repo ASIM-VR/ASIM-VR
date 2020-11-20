219c32fdf55aff7b49329b1690b059265d0f12ab
Jerkko Viisteensaari <vjerkko@gmail.com>
2020-11-20 21:39:16 +0200

    Added Scene, scripts, prefab and materials

---
+++ Assets/ASIM-VR/Examples/Scripts/RadialSection.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+[Serializable]
+public class RadialSection
+{
+    public Sprite icon = null;
+    public SpriteRenderer iconRenderer = null;
+    //For hooking up functionality in editor
+    public UnityEvent onPress = new UnityEvent();
+}
