c4a0109f75096a5cfcaef04fa5da4d7bdfcfca2e
Seppo Kolehmainen <kolehmainen.seppo@gmail.com>
2020-11-24 10:28:55 +0200

    Info display

---
+++ Assets/ASIM-VR/Scripts/InfoDisplay/InfoDisplay.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class InfoDisplay : MonoBehaviour
+{
+    private TextMeshProUGUI textField;
+    private static InfoDisplay _Instance;
+    // Start is called before the first frame update
+
+    public static InfoDisplay Instance
+    {
+        get
+        {
+            return _Instance;
+        }
+        set
+        {
+            if (value != null)
+            {
+                _Instance = value;
+            }
+        }
+    }
+
+    void Awake()
+    {
+        if (Instance != null)
+        {
+            Debug.LogError("InfoDisplay instance already set!", Instance);
+            return;
+        }
+
+        Instance = this;
+        textField = GetComponentInChildren<TextMeshProUGUI>();
+        ClearText();
+    }
+
+    public void SetText(params string[] strArray)
+    {
+        textField.SetText(string.Join("\r\n", strArray));
+    }
+
+
+    public void ClearText()
+    {
+        textField.SetText("");
+    }
+
+}
