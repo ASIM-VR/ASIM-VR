f7bbfa66b14195b49863ac68aaf5e4334a63a4f2
Seppo Kolehmainen <kolehmainen.seppo@gmail.com>
2020-11-24 14:17:35 +0200

    Tool manager

---
+++ Assets/ASIM-VR/Scripts/ToolManager/ToolManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public enum AsimTool
+{
+    //Lisää tähän kaikki työkalut
+    None,
+    ToolPlaceholder,
+}
+
+
+public class ToolManager : MonoBehaviour
+{
+    private List<Tool> Tools;
+
+    void Awake()
+    {
+        Tools = new List<Tool>();
+
+        Tool[] tmp_tools = FindObjectsOfType<Tool>();
+
+        Debug.Log("tmp_tools length" + tmp_tools.Length);
+
+        foreach(Tool tool in tmp_tools)
+        {
+            Tools.Add(tool);
+        }
+
+        DeactivateTools();
+    }
+
+    private void ActivateTool(AsimTool tool) {
+
+        foreach (Tool nTool in Tools)
+        {
+            nTool.gameObject.SetActive(nTool.Type.Equals(tool));
+        }
+    }
+    private void DeactivateTools()
+    {
+        foreach (Tool nTool in Tools)
+        {
+            nTool.gameObject.SetActive(false);
+        }
+    }
+    
+    void Update()
+    {
+        // Temporary test functionality
+        if (Input.GetKeyDown("1"))
+        {
+            ActivateToolPlaceholder();
+        }
+        else if (Input.GetKeyDown("0"))
+        {
+            DeactivateTools();
+        }
+    }
+
+    // Tämä tarvii tehdä jokaiselle työkalulle
+    public void ActivateToolPlaceholder()
+    {
+        ActivateTool(AsimTool.ToolPlaceholder);
+    }
+
+
+}
