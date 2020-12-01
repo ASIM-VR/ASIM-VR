9bd938bcc3ae7cbfbbb92eb532e5c3d1bbad03ac
Nupsi <nupsi4.thenoob@gmail.com>
2020-12-01 12:50:37 +0200

    Updated info display to use VR canvas and refactored game object size

--- Assets/ASIM-VR/Scripts/Measurement/GameObjectSize.cs
+++ Assets/ASIM-VR/Scripts/Measurement/GameObjectSize.cs
@@ -1,8 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.XR.Interaction.Toolkit;
+﻿using UnityEngine;
 using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class GameObjectSize : Tool
 {
@@ -15,38 +13,30 @@ public class GameObjectSize : Tool
     [SerializeField]
     private XRController controller;
 
-    private bool value;
-
-    bool GetXRInputPress()
+    private bool GetXRInputPress()
     {
-        value = false;
-        bool pressed = controller.inputDevice.TryGetFeatureValue(CommonUsages.triggerButton, out value);
-        Debug.Log("PRESSED!");
-        return pressed && value;
+        return controller.inputDevice.TryGetFeatureValue(CommonUsages.triggerButton, out var value) && value;
     }
 
-    void SearchCalculableObject() 
+    private void SearchCalculableObject()
     {
-            RaycastHit hit;
-            controllerRay.GetCurrentRaycastHit(out hit);
-
-            if (hit.transform == true)
+        if(controllerRay.GetCurrentRaycastHit(out var hit))
+        {
+            //If Component that has been hit with ray has certain tag that implies that its size
+            //would like to be calculated then it shows the user x, y and z as cm
+            if(hit.transform.gameObject.CompareTag("Measurable"))
             {
-                //If Component that has been hit with ray has certain tag that implies that its size 
-                //would like to be calculated then it shows the user x, y and z as cm
-                if (hit.transform.gameObject.CompareTag("Measurable"))
-                {
-                    PrintSize(hit);
-                }
+                PrintSize(hit);
             }
+        }
     }
 
     //function that will set text that will be shown to user to the correct values
-    void PrintSize(RaycastHit hit) 
+    private void PrintSize(RaycastHit hit)
     {
         //holds x,y and z which are measures of object
         Vector3 objectSize = hit.transform.gameObject.GetComponent<Renderer>().bounds.size;
-        
+
         InfoDisplay.Instance.SetText(
             hit.transform.gameObject.name + ":",
             "Width: " + objectSize.x.ToString("F2") + "m",
@@ -55,13 +45,14 @@ public class GameObjectSize : Tool
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         //If certain button is pressed Display text will be set to empty strings
-        if (Input.GetMouseButtonDown(1) || controller.inputDevice.TryGetFeatureValue(CommonUsages.secondaryButton, out value)) {
+        if(Input.GetMouseButtonDown(1) || controller.inputDevice.TryGetFeatureValue(CommonUsages.secondaryButton, out var value))
+        {
             InfoDisplay.Instance.ClearText();
         }
-        else if (Input.GetMouseButtonDown(0) || GetXRInputPress())
+        else if(Input.GetMouseButtonDown(0) || GetXRInputPress())
         {
             SearchCalculableObject();
         }
