64b1e297618f09b4cba946d4a7ff46c176177310
Nupsi <nupsi4.thenoob@gmail.com>
2020-12-08 11:09:21 +0200

    Updated radial menu to use AsimInput

--- Assets/ASIM-VR/Scripts/RadialMenu/RMInputManager.cs
+++ Assets/ASIM-VR/Scripts/RadialMenu/RMInputManager.cs
@@ -1,40 +1,63 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using AsimVr.Inputs;
 using UnityEngine;
 using UnityEngine.XR;
 using UnityEngine.XR.Interaction.Toolkit;
-using System.Linq;
 
 public class RMInputManager : MonoBehaviour
 {
     [Header("Scene objects")]
     public RadialMenu RadialMenu = null;
-    
+
     [SerializeField]
-    private XRController controller;
+    private XRNode hand = XRNode.LeftHand;
 
-    void Update()
+    private void OnEnable()
     {
+        AsimInput.Instance.AddListener(InputHelpers.Button.Primary2DAxisTouch, AsimState.Down, ShowRadialMenu);
+        AsimInput.Instance.AddListener(InputHelpers.Button.Primary2DAxisTouch, AsimState.Up, HideRadialMenu);
+        AsimInput.Instance.AddListener(InputHelpers.Button.Primary2DAxisTouch, AsimState.Hold, UpdateTouchPosition);
+        AsimInput.Instance.AddListener(InputHelpers.Button.Primary2DAxisClick, AsimState.Down, SelectActive);
+    }
 
+    private void OnDisable()
+    {
+        AsimInput.Instance.RemoveListener(InputHelpers.Button.Primary2DAxisTouch, AsimState.Down, ShowRadialMenu);
+        AsimInput.Instance.RemoveListener(InputHelpers.Button.Primary2DAxisTouch, AsimState.Up, HideRadialMenu);
+        AsimInput.Instance.RemoveListener(InputHelpers.Button.Primary2DAxisTouch, AsimState.Hold, UpdateTouchPosition);
+        AsimInput.Instance.RemoveListener(InputHelpers.Button.Primary2DAxisClick, AsimState.Down, SelectActive);
+    }
 
-        //Get boolean type input when joystick/touchpad is touched and activate radial menu 
-        RadialMenu.Show(controller.inputDevice.TryGetFeatureValue(CommonUsages.primary2DAxisTouch, out bool touch) && touch);
-
-        //Get boolean type input when joystick/touchpad is pressed and activate specific radial section 
-        InputFeatureUsage<bool> primary2DAxisClickUsage = CommonUsages.primary2DAxisClick;
-        if (controller.inputDevice.TryGetFeatureValue(primary2DAxisClickUsage, out bool press) && press)
+    private void ShowRadialMenu(XRController controller, XRRayInteractor interactor)
+    {
+        if(controller.controllerNode == hand)
         {
-            RadialMenu.ActivateHighlithedSection();
-            //Debug.Log("primary2DAxisClickUsage:" + press);
-        }
-
-        //Get Vector2 type input from joystick/touchpad when its moved and move cursor on the radial menu
-        InputFeatureUsage<Vector2> primary2DAxisUsage = CommonUsages.primary2DAxis;
-        if (controller.inputDevice.TryGetFeatureValue(primary2DAxisUsage, out Vector2 touchPosition) && touchPosition != Vector2.zero)
-        {
-            RadialMenu.SetTouchPosition(touchPosition);
-            //Debug.Log("Primary2DAxisValue:" + touchPosition);
+            RadialMenu.Show(true);
         }
     }
 
+    private void UpdateTouchPosition(XRController controller, XRRayInteractor interactor)
+    {
+        if(controller.controllerNode == hand &&
+           controller.inputDevice.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 touchPosition) &&
+           touchPosition != Vector2.zero)
+        {
+            RadialMenu.SetTouchPosition(touchPosition);
+        }
+    }
+
+    private void HideRadialMenu(XRController controller, XRRayInteractor interactor)
+    {
+        if(controller.controllerNode == hand)
+        {
+            RadialMenu.Show(false);
+        }
+    }
+
+    private void SelectActive(XRController controller, XRRayInteractor interactor)
+    {
+        if(controller.controllerNode == hand)
+        {
+            RadialMenu.ActivateHighlithedSection();
+        }
+    }
 }
