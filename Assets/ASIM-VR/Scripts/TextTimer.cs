5ac9bd4a40b61d3fcfa5d67258b9b00ce79dea2e
Nupsi <nupsi4.thenoob@gmail.com>
2021-01-10 12:01:32 +0200

    Updated text timer to use coroutine. Updated main scene. Removed TerrainSnap script

--- Assets/ASIM-VR/Scripts/TextTimer.cs
+++ Assets/ASIM-VR/Scripts/TextTimer.cs
@@ -1,39 +1,39 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class TextTimer : MonoBehaviour
 {
     [SerializeField]
-    private InfoDisplay infoDisplay;
-    private bool timerOn = false;
-    [SerializeField]
     private float textFadeTime = 1.5f;
-    private float currentTimer;
 
-    private void Update() {
+    private WaitForSeconds wait;
+    private Coroutine coroutine;
 
-        if (timerOn){
-
-            currentTimer -= Time.deltaTime;
-
-            if (currentTimer < 0) {
-                ResetTimer();
-            }
-
-        } 
-    }
-    
-    private void ResetTimer() {
-        timerOn = false;
-        currentTimer = textFadeTime;
-        infoDisplay.ClearText();
+    private void Awake()
+    {
+        wait = new WaitForSeconds(textFadeTime);
     }
 
-    public void StartTimer(string timerText) {
-        currentTimer = textFadeTime;
-        infoDisplay.SetText("Mode: "+timerText);
-        timerOn = true;
+    private void OnValidate()
+    {
+#if UNITY_EDITOR
+        wait = new WaitForSeconds(textFadeTime);
+#endif
     }
 
+    public void StartTimer(string text)
+    {
+        if(coroutine != null)
+        {
+            StopCoroutine(coroutine);
+        }
+        InfoDisplay.Instance.SetText($"Mode: {text}");
+        coroutine = StartCoroutine(WaitForClearText());
+    }
+
+    private IEnumerator WaitForClearText()
+    {
+        yield return wait;
+        InfoDisplay.Instance.ClearText();
+    }
 }
